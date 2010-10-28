6103ed13b8078088d9b37cb4762fb538fdc96475
Nishiyama Yuya <nsy@ulz.nu>
2010-10-28 19:57:27 +0900

    initial commit.

---
+++ server.js
@@ -0,0 +1,84 @@
+var http = require("http")
+  , util = require("util")
+  , fs = require("fs")
+  , path = require("path")
+  , url = require("url")
+  , formidable = require("formidable")
+  , port = 8000
+  , tmpDir = path.join(__dirname, "tmp")
+  , saveDir = path.join(__dirname, "static/upload")
+
+http.createServer(function(req, res) {
+  switch (url.parse(req.url).pathname) {
+    case "/"      : staticHandler(req, res); break;
+    case "/upload": uploadHandler(req, res); break;
+    default       : defaultHandler(req, res); break;
+  }
+}).listen(port)
+
+util.puts("listening on http://localhost:"+port+"/")
+
+
+// handlers
+function defaultHandler(req, res) {
+  if (url.parse(req.url).pathname.match("^/static/(.*)$"))
+    staticHandler(req, res)
+  else notFound(res)
+}
+
+function staticHandler(req, res) {
+  var file = req.url.replace("/", "")
+    , file =  file === "" ? "index.html" : file
+  fs.readFile(file, function(err, data) {
+    if (err) notFound(res)
+    res.writeHead(200, {"Content-Type": ctypes(file)})
+    res.end(data)
+  })
+}
+
+function uploadHandler(req, res) {
+  var form = new formidable.IncomingForm()
+  , files = []
+  , fields = []
+  form.uploadDir = tmpDir
+  form.keepExtensions = true
+
+  form
+    .on("field", function(name, value) {
+    })
+    .on("file", function(name, file) {
+      var tmpFile = file.path
+        , fileName = path.basename(tmpFile)
+        , saveFile = saveDir+"/"+fileName
+      fs.rename(tmpFile, saveFile, function(err) {
+        if (err) throw err
+      })
+    })
+    .on("end", function() {
+      res.writeHead(200, {"Content-Type": "text/plain"})
+      res.end()
+    })
+  form.parse(req)
+}
+
+// utils
+function notFound(res) {
+  res.writeHead(404, {"Content-Type": "text/plain"})
+  res.end("Page is not found.")
+}
+
+function redirect(res) {
+  res.writeHead(302, {"Location": "/\n\n"})
+  res.end()
+}
+
+function p(x) { console.log(util.inspect(x)) }
+
+function ctypes(file) {
+  var types =
+    { ".html": "text/html"
+    , ".js"  : "application/javascript"
+    , ".swf" : "application/x-shockwave-flash"
+    }
+  return types[path.extname(file)] || 'text/plain'
+}
